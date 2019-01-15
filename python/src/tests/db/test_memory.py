bd3c925bdb1129a5a98d9ecf528645facbdd0f22
Csaba Fülöp <csfulop@users.noreply.github.com>
2019-01-15 23:22:34 +0100

    test double add

--- python/src/tests/db/test_memory.py
+++ python/src/tests/db/test_memory.py
@@ -17,10 +17,23 @@ class TestMemory(TestCase):
         # then
         self.assertEqual(self.adapter.size(), 1)
 
-    def test_add_entry_without_id(self):
+    def test_add_entry_without_id_should_fail(self):
         # given
         entry = Entry()
         # then
         with self.assertRaises(PhonebookDbException):
             # when
             self.adapter.add(entry)
+
+    def test_add_entry_twice_should_replace(self):
+        # given
+        entry = Entry()
+        entry.id = 1234
+        entry.name = "Alice"
+        # when
+        self.adapter.add(entry)
+        entry.name = "Bob"
+        self.adapter.add(entry)
+        # then
+        self.assertEqual(self.adapter.size(), 1)
+        # FIXME: assert with get
