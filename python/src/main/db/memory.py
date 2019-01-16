b555279b406c86bfe20d5f1c46491349c54fff84
Csaba Fülöp <csfulop@users.noreply.github.com>
2019-01-16 22:47:27 +0100

    add and test get for memory db adapter

--- python/src/main/db/memory.py
+++ python/src/main/db/memory.py
@@ -2,21 +2,25 @@ from db.adapter import PhonebookDbAdapter, Entry, PhonebookDbException
 
 
 class MemoryPhonebookDbAdapter(PhonebookDbAdapter):
-    _phonebook = {}
+    def __init__(self) -> None:
+        super().__init__()
+        self._phonebook = {}
 
-    def size(self):
+    def size(self) -> int:
         return len(self._phonebook)
 
-    def add(self, entry: Entry):
+    def add(self, entry: Entry) -> None:
         if not entry.id:
             raise PhonebookDbException("missing id for entry: " + str(entry))
+        if entry.id in self._phonebook:
+            raise PhonebookDbException("id already exists: " + str(entry.id))
         self._phonebook[entry.id] = entry
 
     def remove(self, entry: Entry):
         pass
 
-    def get(self, id):
-        pass
+    def get(self, id) -> Entry:
+        return self._phonebook[id]
 
     def list(self):
         pass
--- python/src/tests/db/test_memory.py
+++ python/src/tests/db/test_memory.py
@@ -6,7 +6,7 @@ from db.memory import MemoryPhonebookDbAdapter
 
 class TestMemory(TestCase):
     def setUp(self):
-        self.adapter = MemoryPhonebookDbAdapter();
+        self.adapter = MemoryPhonebookDbAdapter()
 
     def test_add_entry(self):
         # given
@@ -18,6 +18,7 @@ class TestMemory(TestCase):
         self.assertEqual(self.adapter.size(), 1)
 
     def test_add_entry_without_id_should_fail(self):
+        # FIXME: should generate random id
         # given
         entry = Entry()
         # then
@@ -25,15 +26,21 @@ class TestMemory(TestCase):
             # when
             self.adapter.add(entry)
 
-    def test_add_entry_twice_should_replace(self):
+    def test_add_same_id_twice_should_fail(self):
         # given
-        entry = Entry()
-        entry.id = 1234
-        entry.name = "Alice"
-        # when
-        self.adapter.add(entry)
-        entry.name = "Bob"
-        self.adapter.add(entry)
+        id = 1234
+        entry1 = Entry()
+        entry1.id = id
+        entry1.name = "Alice"
+        entry2 = Entry()
+        entry2.id = id
+        entry2.name = "Bob"
+        self.adapter.add(entry1)
         # then
+        with self.assertRaises(PhonebookDbException):
+            # when
+            self.adapter.add(entry2)
         self.assertEqual(self.adapter.size(), 1)
-        # FIXME: assert with get
+        self.assertEqual(self.adapter.get(id), entry1)
+
+    # FIXME: test get
