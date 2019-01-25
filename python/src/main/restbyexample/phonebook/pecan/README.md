0761825fba14805bf7a45fc77cdb00830504d115
Csaba Fülöp <csfulop@users.noreply.github.com>
2019-01-25 22:24:53 +0100

    pecan: add bookstore quick start example

---
+++ python/src/main/restbyexample/phonebook/pecan/README.md
@@ -0,0 +1,18 @@
+REST API with Pecan
+===================
+
+Quick start example
+-------------------
+https://pecan.readthedocs.io/en/latest/quick_start.html
+
+To create a skeleton with the example:
+`pecan create test_project`
+
+Update `config.py` with the root controller, module names and paths.
+
+To execute the example:
+`pecan serve config.py`
+
+REST example
+------------
+https://pecan.readthedocs.io/en/latest/rest.html
--- python/src/main/restbyexample/phonebook/pecan/controllers/root.py
+++ python/src/main/restbyexample/phonebook/pecan/controllers/root.py
@@ -1,22 +1,58 @@
-from pecan import expose, redirect
-from webob.exc import status_map
+from pecan import abort, expose
+
+# Note: this is *not* thread-safe.  In real life, use a persistent data store.
+BOOKS = {
+    '0': 'The Last of the Mohicans',
+    '1': 'Catch-22'
+}
 
 
 class RootController(object):
 
-    @expose(generic=True, template='index.html')
+    @expose()
+    def _lookup(self, id_, *remainder):
+        return BookController(id_), remainder
+
+    # HTTP GET /
+    @expose(generic=True, template='json')
     def index(self):
+        return [dict(id=k, name=v) for k, v in BOOKS.items()]
+
+    # HTTP POST /
+    @index.when(method='POST', template='json')
+    def index_POST(self, **kw):
+        id_ = str(len(BOOKS))  # FIXME: after delete this is incorrect!
+        BOOKS[id_] = kw['name']
+        return dict(id=id_, name=kw['name'])
+
+
+class BookController(object):
+
+    def __init__(self, id_):
+        self.id_ = id_
+        assert self.book
+
+    @property
+    def book(self):
+        if self.id_ in BOOKS:
+            return dict(id=self.id_, name=BOOKS[self.id_])
+        abort(404) # FIXME: "Error page could not be fetched"
+
+    # HTTP GET /<id>/
+    # FIXME: does not work without trailing slash
+    # FIXME: does not fail for undefined methods (eg. PATCH, POST,...)
+    @expose(generic=True, template='json')
+    def index(self):
+        return self.book
+
+    # HTTP PUT /<id>/
+    @index.when(method='PUT', template='json')
+    def index_PUT(self, **kw):
+        BOOKS[self.id_] = kw['name']
+        return self.book
+
+    # HTTP DELETE /<id>/
+    @index.when(method='DELETE', template='json')
+    def index_DELETE(self):
+        del BOOKS[self.id_]
         return dict()
-
-    @index.when(method='POST')
-    def index_post(self, q):
-        redirect('https://pecan.readthedocs.io/en/latest/search.html?q=%s' % q)
-
-    @expose('error.html')
-    def error(self, status):
-        try:
-            status = int(status)
-        except ValueError:  # pragma: no cover
-            status = 500
-        message = getattr(status_map.get(status), 'explanation', '')
-        return dict(status=status, message=message)
